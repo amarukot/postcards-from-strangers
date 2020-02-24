88f750121b86b724f6b1a27de551cf132eea26de
ALDO_M <aldo.marukot@gmail.com>
2020-02-24 15:33:43 -0500

    favorites

--- pobox_zero/models.py
+++ pobox_zero/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Sender(models.Model):
@@ -8,14 +9,20 @@ class Sender(models.Model):
     def __str__(self):
         return self.name
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
 class Postcard(models.Model):
-    image_url = models.TextField(null=True, max_length=300)
-    image = models.ImageField(default='', upload_to='images', blank=True)
-    heading = models.CharField(default='', max_length=255)
-    message = models.TextField()
-    created = models.DateTimeField(auto_now=True)
-    favorited_by = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='favorited_by', blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    image_url       = models.TextField(null=True, max_length=300)
+    image           = models.ImageField(default='', upload_to='images', blank=True)
+    heading         = models.CharField(default='', max_length=255)
+    message         = models.TextField()
+    created         = models.DateTimeField(auto_now=True)
+    favorited_by    = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='favorited_by', blank=True)
+    author          = models.ForeignKey(User, on_delete=models.CASCADE, related_name='postcards', null=True)
 
 
     # sender = models.ForeignKey(Sender, on_delete=models.CASCADE, related_name='postcards')
