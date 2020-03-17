151283926c7d2845f4d02e655f0bb0a3c3a467f9
ALDO_M <aldo.marukot@gmail.com>
2020-03-17 14:48:22 -0400

    AWS S3 setup

--- pobox_zero_proj/settings.py
+++ pobox_zero_proj/settings.py
@@ -74,10 +74,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pobox_zero_proj.urls'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -172,6 +168,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 # django_heroku.settings(locals())
 
 
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+
 # AWS config
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -180,4 +179,5 @@ S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 # AWS_S3_FILE_OVERWRITE = False
 # AWS_DEFAULT_ACL = None
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
