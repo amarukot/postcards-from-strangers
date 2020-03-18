de60ba98c61aee0f4d74097ecda4bbc51ce130cf
ALDO_M <aldo.marukot@gmail.com>
2020-03-18 19:16:09 -0400

    fixed media and static files

--- pobox_zero/models.py
+++ pobox_zero/models.py
@@ -9,19 +9,17 @@ class Sender(models.Model):
     def __str__(self):
         return self.name
 
+
 class Postcard(models.Model):
-    image_url       = models.TextField(null=True, max_length=300)
-    image           = models.ImageField(default='', upload_to='images', blank=True)
-    heading         = models.CharField(default='', max_length=255)
-    message         = models.TextField()
-    created         = models.DateTimeField(auto_now=True)
-    favorited_by    = models.ManyToManyField(User, related_name='favorited_by', blank=True)
-    author          = models.ForeignKey(User, on_delete=models.CASCADE, related_name='postcards')
-
-
-    # sender = models.ForeignKey(Sender, on_delete=models.CASCADE, related_name='postcards')
-    # removed this relationship, believe it should be anonymous as per User Stories
+    image_url = models.TextField(null=True, max_length=300)
+    image = models.ImageField(default='', upload_to='images', blank=True)
+    heading = models.CharField(default='', max_length=255)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+    favorited_by = models.ManyToManyField(
+        User, related_name='favorited_by', blank=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='postcards')
 
     def __str__(self):
         return self.heading
-
