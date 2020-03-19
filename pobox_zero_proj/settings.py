7b67597fe17ae187304983bc69210514de5a4403
ALDO_M <aldo.marukot@gmail.com>
2020-03-18 21:04:18 -0400

    troublshooting debug false server error 500

--- pobox_zero_proj/settings.py
+++ pobox_zero_proj/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 
 MEDIA_URL = '/media/'
