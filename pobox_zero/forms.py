a354c15f36e0e29829405c288f579cbf338ab678
ALDO_M <aldo.marukot@gmail.com>
2020-07-02 14:57:51 -0400

    cleanup code

--- pobox_zero/forms.py
+++ pobox_zero/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from .models import Postcard, Sender
+from .models import Postcard
+
 
 class PostcardForm(forms.ModelForm):
     class Meta:
         model = Postcard
-        fields = ('heading', 'message','image',)
+        fields = ('heading', 'message', 'image',)
